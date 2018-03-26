d505cf08de7b8ffbffff10d0495550e2ab2fc608
Harvinder Bains <harvey@macmini2.local>
2018-03-26 23:03:48 +0100

    Includes corrections for the Site1 and Site2.conf files. The path was wrong

--- myScript.sh
+++ myScript.sh
@@ -5,8 +5,8 @@ sudo ln -s /vagrant/WebDev-sites/www/Html-site /var/www/site1
 sudo ln -s /vagrant/WebDev-sites/www/NodeJs-site /var/www/site2
 
 
-sudo cp /vagrant/Apache/site1.conf /etc/apache2/sites-available/
-sudo cp /vagrant/Apache/site2.conf /etc/apache2/sites-available/
+sudo cp /vagrant/WebDev-sites/site1.conf /etc/apache2/sites-available/
+sudo cp /vagrant/WebDev-sites/site2.conf /etc/apache2/sites-available/
 
 sudo a2ensite site1.conf site2.conf 
 sudo service apache2 reload
