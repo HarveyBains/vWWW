75b814bded3bcb5f9fcd7034ce2c04f793d3beab
Harvinder Bains <harvey@macmini2.local>
2018-03-26 22:19:23 +0100

    NodeJs-Express site added

---
+++ Apache/www/Nodejs-site/expFirst/readMe.txt
@@ -0,0 +1,2 @@
+Run with: DEBUG=myapp:* npm start
+Browse for site2.com:3000
--- myScript.sh
+++ myScript.sh
@@ -1,8 +1,8 @@
 
 sudo cp /vagrant/.bashrc .
 
-sudo ln -s /vagrant/Apache/www/site1 /var/www/site1
-sudo ln -s /vagrant/Apache/www/site2 /var/www/site2
+sudo ln -s /vagrant/Apache/www/site1 /var/www/Html-site
+sudo ln -s /vagrant/Apache/www/site2 /var/www/NodeJs-site
 
 
 sudo cp /vagrant/Apache/site1.conf /etc/apache2/sites-available/
