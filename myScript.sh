a3f7a3a6d0de53e50bd9db2a1ded2ca43486b4a1
Harvinder Bains <harvey@macmini2.local>
2018-03-26 18:28:20 +0100

    Created WWW structure for host-side web development

--- myScript.sh
+++ myScript.sh
@@ -1,11 +1,9 @@
 
 sudo cp /vagrant/.bashrc .
 
-sudo mkdir /var/www/site1
-sudo mkdir /var/www/site2
+sudo ln -s /vagrant/Apache/www/site1 /var/www/site1
+sudo ln -s /vagrant/Apache/www/site2 /var/www/site2
 
-sudo cp /vagrant/Apache/index1.htm /var/www/site1/index.htm
-sudo cp /vagrant/Apache/index2.htm /var/www/site2/index.htm
 
 sudo cp /vagrant/Apache/site1.conf /etc/apache2/sites-available/
 sudo cp /vagrant/Apache/site2.conf /etc/apache2/sites-available/
