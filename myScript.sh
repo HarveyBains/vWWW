57e7450896370d30df1a67b60e31f0482f3ddf82
Harvinder Bains <harvey@macmini2.local>
2018-03-26 22:38:33 +0100

    WWW folder contains multiple sites ready for development

--- myScript.sh
+++ myScript.sh
@@ -1,8 +1,8 @@
 
 sudo cp /vagrant/.bashrc .
 
-sudo ln -s /vagrant/Apache/www/site1 /var/www/Html-site
-sudo ln -s /vagrant/Apache/www/site2 /var/www/NodeJs-site
+sudo ln -s /vagrant/WebDev-sites/www/Html-site /var/www/site1
+sudo ln -s /vagrant/WebDev-sites/www/NodeJs-site /var/www/site2
 
 
 sudo cp /vagrant/Apache/site1.conf /etc/apache2/sites-available/
